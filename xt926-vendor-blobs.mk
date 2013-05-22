ef5177ff09982a08c6bba86466237a8edc1774f3
dhacker29 <davidhackerdvm@gmail.com>
2013-05-22 17:18:02 -0500

    Remove OmxDec's that depend on dev/pmem_audio

--- xt926-vendor-blobs.mk
+++ xt926-vendor-blobs.mk
@@ -126,10 +126,8 @@ PRODUCT_COPY_FILES += \
     vendor/motorola/xt926/proprietary/lib/libnative_renderer.so:system/lib/libnative_renderer.so \
     vendor/motorola/xt926/proprietary/lib/libnetmgr.so:system/lib/libnetmgr.so \
     vendor/motorola/xt926/proprietary/lib/libNimsWrap.so:system/lib/libNimsWrap.so \
-    vendor/motorola/xt926/proprietary/lib/libOmxAacDec.so:system/lib/libOmxAacDec.so \
     vendor/motorola/xt926/proprietary/lib/libOmxEvrcDec.so:system/lib/libOmxEvrcDec.so \
     vendor/motorola/xt926/proprietary/lib/libOmxQcelp13Dec.so:system/lib/libOmxQcelp13Dec.so \
-    vendor/motorola/xt926/proprietary/lib/libOmxWmaDec.so:system/lib/libOmxWmaDec.so \
     vendor/motorola/xt926/proprietary/lib/libopenssl-smime.so:system/lib/libopenssl-smime.so \
     vendor/motorola/xt926/proprietary/lib/libqc-opt.so:system/lib/libqc-opt.so \
     vendor/motorola/xt926/proprietary/lib/libqdi.so:system/lib/libqdi.so \
